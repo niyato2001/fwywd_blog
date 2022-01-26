0c86ed8db32df0247b236bd57fa47182728797b8
niyato2001 <niyato2001@gmail.com>
2022-01-26 23:55:59 +0900

    ボタンcss完成

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -11,9 +11,9 @@ export default function PageHeader(): JSX.Element {
   return (
     <div className='flex justify-between'>
       <Image src='/logo.png' alt='headerlogo' width={200} height={100} />
-      <div className='flex w-1/6 items-center justify-around'>
-        <button className='w-1/3 rounded-md bg-button-green text-sm text-white'>投稿</button>
-        <button className='w-1/3  rounded-md border border-button-border text-sm text-font-green'>
+      <div className='flex w-1/4 items-center justify-center gap-4'>
+        <button className='h-1/3 w-1/3 rounded-md bg-button-green text-xs text-white'>投稿</button>
+        <button className='h-1/3 w-1/3  rounded-md border-2 border-button-border text-xs text-font-green'>
           ログアウト
         </button>
       </div>
