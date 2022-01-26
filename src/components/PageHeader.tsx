33a685094df770c1b461ed831206d86c8079e98a
niyato2001 <niyato2001@gmail.com>
2022-01-26 23:10:11 +0900

    typescriptと@types/reactインストールしなおしtsconfig.json再度生成

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,3 +1,20 @@
+import Image from 'next/image';
+
+interface Props {
+  src: string;
+  alt: string;
+  width: number;
+  height: number;
+}
+
 export default function PageHeader(): JSX.Element {
-  return <div></div>;
+  return (
+    <div className='flex justify-between'>
+      <Image src='/logo.png' alt='headerlogo' width={200} height={100} />
+      <div className='flex w-1/6 items-center justify-around'>
+        <button>投稿</button>
+        <button>ログアウト</button>
+      </div>
+    </div>
+  );
 }
