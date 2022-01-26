cb0360bc4a81233c3a09c27815b3670bb65a0b2f
niyato2001 <niyato2001@gmail.com>
2022-01-26 15:49:53 +0900

    _app.tsxにLayout反映+エラー修正のためLayout関数の引数にprops:anyを追加

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,3 +1,3 @@
-export default function Layout(): JSX.Element {
+export default function Layout(props: any): JSX.Element {
   return <></>;
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import Layout from '../components/Layout';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
 
 export default MyApp;
