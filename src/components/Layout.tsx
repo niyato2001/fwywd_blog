13aa962fa4f0cdd5e0275dd162dd882cd830821f
niyato2001 <niyato2001@gmail.com>
2022-01-26 16:04:05 +0900

    Layoutにchildrenの型を定義、header/footerをimport

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,3 +1,17 @@
-export default function Layout(props: any): JSX.Element {
-  return <></>;
+import { ReactNode } from 'react';
+import PageFooter from './PageFooter';
+import PageHeader from './PageHeader';
+
+interface Props {
+  children: ReactNode;
+}
+
+export default function Layout({ children }: Props): JSX.Element {
+  return (
+    <>
+      <PageHeader />
+      {children}
+      <PageFooter />
+    </>
+  );
 }
