de8e0ce525a5726bc52248e9e4814ba9ef642a95
niyato2001 <niyato2001@gmail.com>
2022-01-26 23:22:16 +0900

    tailwind.config.jsに色の設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      'font-black': '#384359',
+      'font-green': '#008c8d',
+      'bg-green': '#6bc2c3',
+      'bg-black': '#262c3a',
+      'bg-gray': '#f8fcfe',
+      'button-green': '#2bb9ba',
+      'button-border': '#c5eaea',
+    },
     extend: {},
   },
   plugins: [],
