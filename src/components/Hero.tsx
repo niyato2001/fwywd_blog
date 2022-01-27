e4e5667183e09b9ea63e188f857d0d991f0828a7
niyato2001 <niyato2001@gmail.com>
2022-01-27 16:22:02 +0900

    imageの親要素にposition relativeを追加

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image';
 
 export default function Hero() {
   return (
-    <div className='bg-bg-green text-white'>
-      <Image src='/bg-hero.png' alt='bg_Hero' layout='fill' />
+    <div className=' relative bg-bg-green text-white'>
+      <Image src='/bg-hero.png' alt='bg_Hero' layout='fill' objectFit='cover' />
       <div className='max-w-2lg flex items-center  gap-32 py-10'>
         <div className='mx-auto flex w-1/2 items-center justify-center gap-4'>
           <h1 className='text-4xl font-extrabold'>ブログ</h1>
