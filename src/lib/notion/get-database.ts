0e8c7dbe715db07ef4935424391b5ed3f3325ad1
niyato2001 <niyato2001@gmail.com>
2022-01-30 22:37:06 +0900

    databaseのGET関数

--- src/lib/notion/get-database.ts
+++ src/lib/notion/get-database.ts
@@ -1,36 +1,8 @@
 import { notion } from './client';
 
-interface NotionPage {
-  database: Database;
-
-  // added because page data is too nested
-  id: string;
-  title: string;
-}
-
-interface Database {
-  id: string;
-  properties: {
-    title: {
-      id: string;
-      type: string;
-      title: [
-        {
-          plain_text: string;
-        },
-      ];
-    };
-  };
-}
-
-export async function getDatabase(id: string): Promise<NotionPage> {
-  const database = (await notion.database.query({
-    database_id: id,
-  })) as NotionPage['database'];
-
-  return {
-    database,
-    id: database.id,
-    title: database.properties.title.title[0].plain_text,
-  };
-}
+export const getDatabase = async (databaseId) => {
+  const response = await notion.databases.query({
+    database_id: databaseId,
+  });
+  return response.results;
+};
