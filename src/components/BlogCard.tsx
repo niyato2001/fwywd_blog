59065f4a60473f5374115048b51f601a912d147a
niyato2001 <niyato2001@gmail.com>
2022-01-31 12:07:29 +0900

    BlogCardの体裁を整える。tailwindcssプラグインline-clampをインストール

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -13,7 +13,7 @@ export default function BlogCard(props: Props) {
         <div className='h-44 rounded-t-md bg-bg-gray-dark'></div>
         <div className='flex h-52 flex-col gap-4 rounded-b-md bg-white p-10'>
           <div className='text-bg-gray-dark'>{props.date}</div>
-          <div className='text-font-black'>{props.title}</div>
+          <div className='text-font-black line-clamp-2'>{props.title}</div>
           <div className='flex gap-1 text-bg-gray-dark'>
             <div className='rounded-md border-2 border-bg-gray-dark px-3 py-1 text-xs'>
               {props.tag1}
