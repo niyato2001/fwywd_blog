41d01ba5777d0320f0bd9043487d035ad83ce2d8
niyato2001 <niyato2001@gmail.com>
2022-02-01 22:32:16 +0900

    コメント追加

--- src/pages/posts/[post].tsx
+++ src/pages/posts/[post].tsx
@@ -5,11 +5,13 @@ import { getDatabase } from '../../lib/notion/get-database';
 import { getPage } from '../../lib/notion/get-page';
 
 export default function Post({ title }) {
+  //{title}とすることで、props:{title:.....}のtitleのみを受け取ることができる！
   return <h1>このページは{title}のページです。</h1>;
 }
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const post = context.params.post;
+  //postを{post}としていたために、オブジェクト扱いになってしまっていた！！
   const page = await getPage(post);
   const props = {
     title: `title: ${page.properties.title.title[0].plain_text}`,
@@ -23,6 +25,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
 const databaseId = process.env.NOTION_DATABASE_ID;
 export const getStaticPaths: GetStaticPaths = async () => {
   const database = await getDatabase(databaseId);
+  //databaseはresponse.resultsと同様のオブジェクト
   const paths = database.map((post) => ({
     params: { post: post.id.toString() },
   }));
