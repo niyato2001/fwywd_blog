699c7138b6b0ca4f5459727f4895363f55e842fa
niyato2001 <niyato2001@gmail.com>
2022-02-01 22:18:27 +0900

    get-blocks.tsを追加

---
+++ src/lib/notion/get-blocks.ts
@@ -0,0 +1,9 @@
+import { notion } from './client';
+
+export const getBlocks = async (blockId) => {
+  const response = await notion.blocks.children.list({
+    block_id: blockId,
+    page_size: 50,
+  });
+  return response.results;
+};
