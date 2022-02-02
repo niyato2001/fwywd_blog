7900c9461743029d0ffb15ce821fcc8cf88587da
niyato2001 <niyato2001@gmail.com>
2022-02-02 23:15:37 +0900

    get-blocks関数を作成

--- src/lib/notion/get-blocks.ts
+++ src/lib/notion/get-blocks.ts
@@ -3,7 +3,7 @@ import { notion } from './client';
 export const getBlocks = async (blockId) => {
   const response = await notion.blocks.children.list({
     block_id: blockId,
-    page_size: 50,
+    page_size: 100,
   });
   return response.results;
 };
