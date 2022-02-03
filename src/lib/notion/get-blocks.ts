78e805c41a50b94a02544626bfee69493439f136
niyato2001 <niyato2001@gmail.com>
2022-02-03 16:49:18 +0900

    getBlocksにコメント追加

--- src/lib/notion/get-blocks.ts
+++ src/lib/notion/get-blocks.ts
@@ -1,6 +1,7 @@
 import { notion } from './client';
 
 export const getBlocks = async (blockId) => {
+  //Notion APIではblockIdはpageIdと同じ
   const response = await notion.blocks.children.list({
     block_id: blockId,
     page_size: 100,
