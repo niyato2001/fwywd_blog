517b823b1d9deef16bdf022ba4a3054a163fdfc5
niyato2001 <niyato2001@gmail.com>
2022-02-04 16:48:07 +0900

    <text>を追加とblock childrenを取得してtextをとりあえず表示

--- src/pages/posts/[post].tsx
+++ src/pages/posts/[post].tsx
@@ -1,18 +1,129 @@
 import { data } from 'autoprefixer';
 import { GetStaticProps, GetStaticPaths } from 'next';
+import Image from 'next/image';
 import { useRouter } from 'next/router';
+import { Fragment } from 'react';
 import { getBlocks } from '../../lib/notion/get-blocks';
 import { getDatabase } from '../../lib/notion/get-database';
 import { getPage } from '../../lib/notion/get-page';
 
-export default function Post({ props_page, props_blocks }) {
+export const Text = ({ text }) => {
+  if (!text) {
+    return null;
+  }
+  return text.map((value, i) => {
+    const {
+      annotations: { bold, code, color, italic, strikethrough, underline },
+      text,
+    } = value;
+    return (
+      <span
+        key={i}
+        className={[
+          bold ? 'font-bold' : '',
+          code ? 'bg-gray-light rounded-3xl px-2 py-4 font-mono' : '',
+          italic ? 'italic' : '',
+          strikethrough ? 'line-through' : '',
+          underline ? 'underline' : '',
+        ].join(' ')}
+        style={color !== 'default' ? { color } : {}}
+      >
+        {text.link ? <a href={text.link.url}>{text.content}</a> : text.content}
+      </span>
+    );
+  });
+};
+
+const renderBlock = (block) => {
+  const { type, id } = block;
+  const value = block[type];
+
+  switch (type) {
+    case 'paragraph':
+      return (
+        <p>
+          <Text text={value.text} />
+        </p>
+      );
+    case 'heading_1':
+      return (
+        <h1>
+          <Text text={value.text} />
+        </h1>
+      );
+    case 'heading_2':
+      return (
+        <h2>
+          <Text text={value.text} />
+        </h2>
+      );
+    case 'heading_3':
+      return (
+        <h3>
+          <Text text={value.text} />
+        </h3>
+      );
+    case 'bulleted_list_item':
+    case 'numbered_list_item':
+      return (
+        <li>
+          <Text text={value.text} />
+        </li>
+      );
+    case 'to_do':
+      return (
+        <div>
+          <label htmlFor={id}>
+            <input type='checkbox' id={id} defaultChecked={value.checked} />{' '}
+            <Text text={value.text} />
+          </label>
+        </div>
+      );
+    case 'toggle':
+      return (
+        <details>
+          <summary>
+            <Text text={value.text} />
+          </summary>
+          {value.children?.map((block) => (
+            <Fragment key={block.id}>{renderBlock(block)}</Fragment>
+          ))}
+        </details>
+      );
+    case 'child_page':
+      return <p>{value.title}</p>;
+    //      case 'image':
+    //        const src = value.type === 'external' ? value.external.url : value.file.url;
+    //        const caption = value.caption ? value.caption[0].plain_text : '';
+    //        return (
+    //          <figure>
+    //            <Image src={src} alt={caption} />
+    //            {caption && <figcaption>{caption}</figcaption>}
+    //          </figure>
+    //        );
+    default:
+      //      return `❌ Unsupported block (${
+      //        type === 'unsupported' ? 'unsupported by Notion API' : type
+      //      })`;
+      return null;
+  }
+};
+
+export default function Post({ props_page, blocks }) {
+  if (!props_page || !blocks) {
+    return <div />;
+  }
   //{title}とすることで、props:{title:.....}のtitleのみを受け取ることができる！
   //retrieve a pageとretrieve blocks childrenの両方を受け取る
   return (
-    <h1>
-      このページは{props_page.title}のページです。 1つめのブロックのタイプは{props_blocks.type}
-      です。
-    </h1>
+    <article>
+      <h1>{props_page.title}</h1>
+      <section>
+        {blocks.map((block) => (
+          <Fragment key={block.id}>{renderBlock(block)}</Fragment>
+        ))}
+      </section>
+    </article>
   );
 }
 
@@ -24,14 +135,28 @@ export const getStaticProps: GetStaticProps = async (context) => {
   const page = pageAndBlocks[0];
   const blocks = pageAndBlocks[1];
   const props_page = {
-    title: `title: ${page.properties.title.title[0].plain_text}`,
+    title: '{page.properties.title.title[0].plain_text}',
   };
   //props→props_page
-  const props_blocks = {
-    type: `type: ${blocks[0].type}`,
-  };
+  const childBlocks = await Promise.all(
+    blocks
+      .filter((block) => block.has_children)
+      .map(async (block) => {
+        return {
+          id: block.id,
+          children: await getBlocks(block.id),
+        };
+      }),
+  );
+  const blocksWithChildren = blocks.map((block) => {
+    // Add child blocks if the block should contain children but none exists
+    if (block.has_children && !block[block.type].children) {
+      block[block.type]['children'] = childBlocks.find((x) => x.id === block.id)?.children;
+    }
+    return block;
+  });
   return {
-    props: { props_page, props_blocks },
+    props: { props_page, blocks: blocksWithChildren },
     //props→props:{props_page,props_blocks}に変更
     revalidate: 10,
   };
