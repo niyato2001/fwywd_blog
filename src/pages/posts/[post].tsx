ff5785fa1353f7788c4bdfd2836337a3f221183a
niyato2001 <niyato2001@gmail.com>
2022-02-06 17:14:11 +0900

    [post]にコメントアウトを追加

--- src/pages/posts/[post].tsx
+++ src/pages/posts/[post].tsx
@@ -8,6 +8,7 @@ import { getDatabase } from '../../lib/notion/get-database';
 import { getPage } from '../../lib/notion/get-page';
 
 export const Text = ({ text }) => {
+  //text=[]でtextは配列オブジェクト！
   if (!text) {
     return null;
   }
@@ -16,6 +17,24 @@ export const Text = ({ text }) => {
       annotations: { bold, code, color, italic, strikethrough, underline },
       text,
     } = value;
+    //textは[
+    //{"type": "text",
+    //  "text": {
+    //      "content": "APIを使ってデータを○○する【GET/POST/PUT/DELETE】",
+    //      "link": null
+    //  },
+    //  "annotations": {
+    //      "bold": false,
+    //      "italic": false,
+    //      "strikethrough": false,
+    //      "underline": false,
+    //      "code": false,
+    //      "color": "default"
+    //  },
+    //  "plain_text": "APIを使ってデータを○○する【GET/POST/PUT/DELETE】",
+    //  "href": null
+    //}
+    //]のような配列構造で、そのうちのtextとannotationに対して分割代入している。
     return (
       <span
         key={i}
@@ -29,14 +48,69 @@ export const Text = ({ text }) => {
         style={color !== 'default' ? { color } : {}}
       >
         {text.link ? <a href={text.link.url}>{text.content}</a> : text.content}
+        {/*text.linkがtrueならばaタグ、そうでなければ単純にtext.contentのspanタグ*/}
       </span>
     );
   });
 };
 
 const renderBlock = (block) => {
+  //blockはオブジェクト
+  //block={
+  //{
+  //  "object": "block",
+  //"id": "1d95866b-810a-49b7-9421-a52935fc754b",
+  //      "created_time": "2022-01-25T06:16:00.000Z",
+  //    "last_edited_time": "2022-01-25T06:16:00.000Z",
+  //   "has_children": false,
+  //    "archived": false,
+  //    "type": "paragraph",
+  //    "paragraph": {
+  //    "children":あれば子ブロック,
+  //        "text": [
+  //            {
+  //                "type": "text",
+  //               "text": {
+  //                 "content": "【目次】",
+  //               "link": null
+  //         },
+  //       "annotations": {
+  //         "bold": true,
+  //       "italic": false,
+  //     "strikethrough": false,
+  //        "underline": false,
+  //             "code": false,
+  //           "color": "default"
+  //     },
+  //        "plain_text": "【目次】",
+  //        "href": null
+  //    },
+  //    {
+  //           "type": "text",
+  //                  "text": {
+  //                    "content": "                                                                                                                  written by 田原純平",
+  //                  "link": null
+  //            },
+  //          "annotations": {
+  //            "bold": false,
+  //          "italic": false,
+  //        "strikethrough": false,
+  //      "underline": false,
+  //    "code": false,
+  //      "color": "default"
+  //      },
+  //     "plain_text": "                                                                                                                  written by 田原純平",
+  //    "href": null
+  //      }
+  //     ]
+  //    }
+  // }
+  //}のような形。
   const { type, id } = block;
+  //const type = block.type;
+  //const id = block.id;　と同じ。分割代入。
   const value = block[type];
+  //value=block[block.type]という意味。たとえば、value=block['paragraph']
 
   switch (type) {
     case 'paragraph':
@@ -141,6 +215,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
   const pageAndBlocks = await Promise.all([getPage(post), getBlocks(post)]);
   const page = pageAndBlocks[0];
   const blocks = pageAndBlocks[1];
+  //blocksはparagraphなどのtypeごとの配列
   const props_page = {
     title: page.properties.title.title[0].plain_text,
   };
@@ -148,7 +223,9 @@ export const getStaticProps: GetStaticProps = async (context) => {
   const childBlocks = await Promise.all(
     blocks
       .filter((block) => block.has_children)
+      //block.has_childrenがtrueの要素のみの配列に変換
       .map(async (block) => {
+        //子ブロックがある場合は、内容がすべて表示されるわけではないので、追加でblock_idからgetBlocksする必要がある
         return {
           id: block.id,
           children: await getBlocks(block.id),
@@ -158,13 +235,19 @@ export const getStaticProps: GetStaticProps = async (context) => {
   const blocksWithChildren = blocks.map((block) => {
     // Add child blocks if the block should contain children but none exists
     if (block.has_children && !block[block.type].children) {
+      //has_childrenがtrueだが、childrenオブジェクトを持っていない場合にchildrenオブジェクトをつくる！
       block[block.type]['children'] = childBlocks.find((x) => x.id === block.id)?.children;
+      //xはblocks.filterで作られたblock.has_childrenがtrueのみのblockによる配列の要素（idとchildren）
+      //childBlocks=[{id:block.id,children:await getBlocks(block.id),{id:...,children:...},...]
+      //x.idはchildBlocksで呼び出されているblock.idであり、あとのblock.idはblocksSithChildrenで呼び出されているblock.id
+      //".?"はオプショナルチェーン演算子とよび、childblocks.find()がある場合にchildrenを呼び出す。childblocks.find()がnullの場合はundefinedを返す。
     }
     return block;
+    //has_childrenがtrueのものは、childrenが加えられた状態でfalseのものはそのままreturnされる。
   });
   return {
     props: { props_page, blocks: blocksWithChildren },
-    //props→props:{props_page,props_blocks}に変更
+    //pageとデータベースの2つの情報を入れるためprops→props:{props_page,props_blocks}に変更
     revalidate: 10,
   };
 };
