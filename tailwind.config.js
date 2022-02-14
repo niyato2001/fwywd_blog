5ec3f295f5241db9be397ead8336026e689f6063
niyato2001 <niyato2001@gmail.com>
2022-02-14 11:01:03 +0900

    jit modeを追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
+  mode: 'jit',
   content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     colors: {
