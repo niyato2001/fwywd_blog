e114f51fdecca67c424ff3988e5b0ca4f7bf087a
niyato2001 <niyato2001@gmail.com>
2022-02-15 11:37:13 +0900

    SelectButtonコンポーネントを変更

--- src/components/SelectButton.tsx
+++ src/components/SelectButton.tsx
@@ -4,13 +4,12 @@ export default function SelectButton() {
   const router = useRouter();
   return (
     <select
-      className='rounded-md p-4 px-20 text-center'
+      className='rounded-md p-4 px-20 text-center text-font-black'
       onChange={(e) => Router.push(e.target.value)}
       defaultValue={router.pathname}
     >
       <option value='/'>Home</option>
-      <option value='/about'>About</option>
-      <option value='/profile'>Profile</option>
+      <option value='/blogtables'>ページ1</option>
     </select>
   );
 }
