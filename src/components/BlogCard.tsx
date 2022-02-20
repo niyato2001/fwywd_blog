01a5c2cc8839d7928c9005e08279a9bece963ec4
niyato2001 <niyato2001@gmail.com>
2022-02-20 14:36:23 +0900

    2)BlogCardコンポーネントで記事のImageを設定、objectFitが大切

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 interface Props {
@@ -5,14 +6,15 @@ interface Props {
   date: string;
   tag: string[];
   href: string;
+  image: string;
 }
 
 export default function BlogCard(props: Props) {
   return (
     <Link href={`/posts/${props.href}`}>
-      <a className='flex max-h-96 max-w-md flex-col'>
-        <div className='h-44 rounded-t-md bg-bg-gray-dark'></div>
-        <div className='flex h-52 flex-col gap-4 rounded-b-md bg-white p-10'>
+      <a className='flex h-auto max-w-md flex-col divide-y-2'>
+        <Image src={props.image} alt='article_image' width={480} height={270} objectFit='contain' />
+        <div className='flex h-52 flex-col gap-4 rounded-b-md bg-white p-8'>
           <div className='text-bg-gray-dark'>{props.date}</div>
           <div className='text-font-black line-clamp-2'>{props.title}</div>
           <div className='flex gap-1 text-bg-gray-dark'>
