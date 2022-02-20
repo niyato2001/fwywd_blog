cf2d590982eb6b5b20f4d227c8093d50678ca85e
niyato2001 <niyato2001@gmail.com>
2022-02-20 15:11:48 +0900

    index.tsxからPageMainにBlogCardを移動

--- src/components/PageMain.tsx
+++ src/components/PageMain.tsx
@@ -1,13 +1,10 @@
 import Image from 'next/image';
+import SelectButton from '../components/SelectButton';
+import PageList from '../lib/pagenation/PageList';
 import BlogCard from './BlogCard';
 
-interface Props {
-  title: string;
-  date: string;
-  tag: string;
-}
-
-export default function PageMain(): JSX.Element {
+export default function PageMain(props): JSX.Element {
+  const pageList: string[] = PageList(props.posts);
   return (
     <div className='bg-bg-gray-light'>
       <div className='mx-auto flex max-w-5xl flex-col py-16'>
@@ -15,15 +12,27 @@ export default function PageMain(): JSX.Element {
           <div className='inline-block rounded-md border-2 border-button-border px-3 py-1 text-xs text-font-green'>
             プログラミング
           </div>
-          <div className='inline-block font-bold text-font-black'>1/26 ページ</div>
+          <div className='inline-block font-bold text-font-black'>記事一覧</div>
         </div>
-        <div className='grid grid-cols-3 grid-rows-2 gap-10 py-16'></div>
+        <div className='grid grid-cols-3 grid-rows-2 gap-10 py-16'>
+          {props.posts.map((post, i) => {
+            return (
+              <BlogCard
+                key={i}
+                title={post.properties.title.title[0].plain_text}
+                date={post.properties.date.date.start}
+                tag={post.properties.tag.multi_select.map((tag) => {
+                  return tag.name;
+                })}
+                href={post.id}
+                image={post.properties.image?.files[0].file.url}
+              />
+            );
+          })}
+        </div>
         <div className='mx-auto flex items-center justify-center gap-1   text-white'>
           <button className='rounded-l-md bg-button-green p-3'>前へ</button>
-          <button className='flex justify-between gap-4 bg-button-green py-3 pl-10 pr-4'>
-            <div>1/26</div>
-            <div>v</div>
-          </button>
+          <SelectButton numbers={pageList} />
           <button className='rounded-r-md bg-button-green p-3'>次へ</button>
         </div>
       </div>
