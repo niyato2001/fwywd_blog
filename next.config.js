0f5f5591bf01523ce9729585abcb9c483351f838
niyato2001 <niyato2001@gmail.com>
2022-02-22 13:27:56 +0900

    svg画像取得のための対応

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     domains: ['s3.us-west-2.amazonaws.com'],
   },
   reactStrictMode: true,
