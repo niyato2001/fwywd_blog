c920a2b881f3ac87c5bf71ba5278050db36f5b82
niyato2001 <niyato2001@gmail.com>
2022-02-24 23:36:45 +0900

    add-block使用できず。。

--- src/lib/notion/add-blocks.ts
+++ src/lib/notion/add-blocks.ts
@@ -1,25 +1,25 @@
-import { getBlocks } from './get-blocks';
+import { getOgpMeta } from './get-embedOGP';
 
-export async function addBlocks(blocks, blockType) {
+export async function addBlocks(blocks, blocktype, blockkey) {
   const addBlocks = await Promise.all(
     blocks
-      .filter((block) => block[blockType])
-      //block.has_childrenがtrueの要素のみの配列に変換
+      .filter((block) => block[blocktype])
+      //block[blockType]がtrueの要素のみの配列に変換
       .map(async (block) => {
         //子ブロックがある場合は、内容がすべて表示されるわけではないので、追加でblock_idからgetBlocksする必要がある
         return {
           id: block.id,
-          blockType: await getBlocks(block.id),
+          target: await getOgpMeta(block[blocktype][blockkey]),
         };
       }),
   );
 
-  const blocksWithChildren = addBlocks.map((block) => {
+  const blocksWithAdd = blocks.map((block) => {
     // Add child blocks if the block should contain children but none exists
-    if (block?.blockType && !block?.blockType.blockType) {
+    if (block[blocktype] && !block[blocktype]?.target) {
       //has_childrenがtrueだが、childrenオブジェクトを持っていない場合にchildrenオブジェクトをつくる！
-      block[block.type][blockType] = addBlocks.find((x) => x.id === block.id)?.children;
-      //xはblocks.filterで作られたblock.has_childrenがtrueのみのblockによる配列の要素（idとchildren）
+      block[blocktype].target = addBlocks.find((x) => x.id === block.id)?.target;
+      //xはblocks.filterで作られたblock.has_childrenがtrueのみのblockによる配列の要素（idとtarget）
       //childBlocks=[{id:block.id,children:await getBlocks(block.id),{id:...,children:...},...]
       //x.idはchildBlocksで呼び出されているblock.idであり、あとのblock.idはblocksで呼び出されているblock.id
       //"?."はオプショナルチェーン演算子とよび、childblocks.find()がある場合にchildrenを呼び出す。childblocks.find()がnullの場合はundefinedを返す。
@@ -27,4 +27,5 @@ export async function addBlocks(blocks, blockType) {
     return block;
     //has_childrenがtrueのものは、childrenが加えられた状態でfalseのものはそのままreturnされる。
   });
+  return blocksWithAdd;
 }
