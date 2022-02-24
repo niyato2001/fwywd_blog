39cc67b9b5d1c363b6ae28316e0fddf03b5b5848
niyato2001 <niyato2001@gmail.com>
2022-02-24 16:19:35 +0900

    getしたOGPのcssを改良

--- src/components/LinkCard.tsx
+++ src/components/LinkCard.tsx
@@ -10,21 +10,23 @@ interface Props {
 
 export default function LinkCard(props: Props) {
   return (
-    <Link href={props.url}>
-      <a className='grid grid-cols-5 rounded-md bg-white p-3'>
-        <div className='relative col-span-1 flex items-center justify-start'>
-          <Image
-            src={props.image ? props.image : '/vercel.svg'}
-            alt={props.title}
-            layout='fill'
-            objectFit='contain'
-          />
-        </div>
-        <div className='col-span-4 flex flex-col justify-start'>
-          <div className='text-black text-xl font-bold'>{props.title}</div>
-          <div className='text-xs text-gray-400'>{props.description}</div>
-        </div>
-      </a>
-    </Link>
+    <div className='rounded-md border-2 p-1'>
+      <Link href={props.url}>
+        <a className='grid grid-cols-5 rounded-md bg-white p-3'>
+          <div className='relative col-span-1 flex items-center justify-start'>
+            <Image
+              src={props.image ? props.image : '/vercel.svg'}
+              alt={props.title}
+              layout='fill'
+              objectFit='contain'
+            />
+          </div>
+          <div className='col-span-4 flex flex-col justify-start divide-y-2 px-2'>
+            <div className='text-black p-1 text-lg font-bold'>{props.title}</div>
+            <div className='p-1 text-xs text-gray-800'>{props.description}</div>
+          </div>
+        </a>
+      </Link>
+    </div>
   );
 }
