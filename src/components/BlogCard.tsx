beca83f0b42570d0dade812baa4326c0802ac819
niyato2001 <niyato2001@gmail.com>
2022-03-08 19:26:56 +0900

    blogcardのタグ表示をflex-wrapに変更

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -14,13 +14,16 @@ export default function BlogCard(props: Props) {
     <Link href={`/posts/${props.href}`}>
       <a className='flex h-auto max-w-md flex-col divide-y-2'>
         <Image src={props.image} alt='article_image' width={480} height={270} objectFit='contain' />
-        <div className='flex h-52 flex-col gap-4 rounded-b-md bg-white p-8'>
+        <div className='flex h-64  flex-col  gap-6 rounded-b-md bg-white px-8 py-6'>
           <div className='text-bg-gray-dark'>{props.date}</div>
           <div className='text-font-black line-clamp-2'>{props.title}</div>
-          <div className='flex gap-1 text-bg-gray-dark'>
+          <div className='flex flex-wrap  gap-1 text-bg-gray-dark'>
             {props.tag.map((tag, i) => {
               return (
-                <div key={i} className='rounded-md border-2 border-bg-gray-dark px-3 py-1 text-xs'>
+                <div
+                  key={i}
+                  className=' rounded-md border-2 border-bg-gray-dark px-3 py-1 text-center text-xs'
+                >
                   {tag}
                 </div>
               );
