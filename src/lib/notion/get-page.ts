eadc5a23acab7afe7b3083d2ababe7f003fb444e
niyato2001 <niyato2001@gmail.com>
2022-03-08 10:34:39 +0900

    get-page.tsをタイプスクリプト化

--- src/lib/notion/get-page.ts
+++ src/lib/notion/get-page.ts
@@ -1,6 +1,6 @@
 import { notion } from './client';
 
-export async function getPage(pageId) {
+export async function getPage(pageId: string) {
   const page = await notion.pages.retrieve({
     page_id: pageId,
   });
