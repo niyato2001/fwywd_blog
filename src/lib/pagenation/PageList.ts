544f583295e0dd1974da8f992a958606113d9c94
niyato2001 <niyato2001@gmail.com>
2022-03-08 11:06:34 +0900

    PageList.tsをタイプスクリプト化

--- src/lib/pagenation/PageList.ts
+++ src/lib/pagenation/PageList.ts
@@ -1,4 +1,86 @@
-export default function PageList(page) {
+interface Page {
+  object: string;
+  id: string;
+  created_time: Date;
+  last_edited_time: Date;
+  created_by: CreatedBy;
+  last_edited_by: LastEditedBy;
+  cover: Cover;
+  icon?: any;
+  parent: Parent;
+  archived: boolean;
+  properties: Properties;
+  url: string;
+}
+
+interface CreatedBy {
+  object: string;
+  id: string;
+}
+interface LastEditedBy {
+  object: string;
+  id: string;
+}
+interface Cover {
+  type: string;
+  external: External;
+}
+interface External {
+  url: string;
+}
+interface Parent {
+  type: string;
+  database_id: string;
+}
+interface Properties {
+  tag: Tag;
+  date: Date;
+  link: Link;
+  image: Image;
+  title: Title;
+}
+interface Tag {
+  id: string;
+  type: string;
+  multi_select: MultiSelect[];
+}
+interface MultiSelect {
+  id: string;
+  name: string;
+  color: string;
+}
+interface Link {
+  id: string;
+  type: string;
+  url: string;
+}
+interface Image {
+  id: string;
+  type: string;
+  files: File[];
+}
+interface Title {
+  id: string;
+  type: string;
+  title: Title2[];
+}
+interface Title2 {
+  type: string;
+  text: Text;
+  annotations: Annotations;
+  plain_text: string;
+  href: string | null;
+}
+interface Annotations {
+  bold: boolean;
+  italic: boolean;
+  strikethrough: boolean;
+  underline: boolean;
+  code: boolean;
+  color: string;
+}
+
+export default function PageList(page: Page[]) {
   const pageNumber: number = Math.floor(page.length / 6);
   const pageList: string[] = [];
   if (page.length % 6 === 0) {
