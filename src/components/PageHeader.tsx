239c0293b6ba1497deea10ef1bc730290d49f40c
niyato2001 <niyato2001@gmail.com>
2022-03-18 23:01:15 +0900

    ログインボタンの設定

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,4 +1,6 @@
+import { useMsal } from '@azure/msal-react';
 import Image from 'next/image';
+import { loginRequest } from '../lib/auth/config';
 
 interface Props {
   src: string;
@@ -8,13 +10,17 @@ interface Props {
 }
 
 export default function PageHeader(): JSX.Element {
+  const { instance } = useMsal();
   return (
     <div className='flex justify-between'>
       <Image src='/logo.png' alt='headerlogo' width={200} height={100} />
       <div className='flex w-1/4 items-center justify-center gap-4'>
         <button className='h-1/3 w-1/3 rounded-md bg-button-green text-xs text-white'>投稿</button>
-        <button className='h-1/3 w-1/3  rounded-md border-2 border-button-border text-xs text-font-green'>
-          ログアウト
+        <button
+          className='h-1/3 w-1/3 rounded-md border-2 border-button-border  text-xs text-font-green'
+          onClick={() => instance.loginRedirect(loginRequest)}
+        >
+          ログイン
         </button>
       </div>
     </div>
--- src/lib/auth/config.ts
+++ src/lib/auth/config.ts
@@ -2,8 +2,7 @@ import type { Configuration } from '@azure/msal-browser';
 export const msalConfig: Configuration = {
   auth: {
     clientId: '8a953fbd-b7f3-41ab-84df-b3e4fa4ab20a',
-    authority:
-      'https://fwywdblog.b2clogin.com/fwywdblog.onmicrosoft.com/oauth2/v2.0/authorize?p=b2c_1_signin',
+    authority: 'https://fwywdblog.b2clogin.com/fwywdblog.onmicrosoft.com/b2c_1_signin',
     knownAuthorities: ['fwywdblog.b2clogin.com'],
     redirectUri: '/',
     postLogoutRedirectUri: '/',
