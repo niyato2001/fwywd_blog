710ef6d74815688ccbea91d21f98479c9b2fec7e
niyato2001 <niyato2001@gmail.com>
2022-03-18 22:28:22 +0900

    config設定追加

--- src/lib/auth/config.ts
+++ src/lib/auth/config.ts
@@ -1,8 +1,9 @@
 export const msalConfig = {
   auth: {
     clientId: '8a953fbd-b7f3-41ab-84df-b3e4fa4ab20a',
-    authority: '',
-    knownAuthorities: [],
+    authority:
+      'https://fwywdblog.b2clogin.com/fwywdblog.onmicrosoft.com/oauth2/v2.0/authorize?p=b2c_1_signin',
+    knownAuthorities: ['fwywdblog.b2clogin.com'],
     redirectUri: '/',
     postLogoutRedirectUri: '/',
   },
